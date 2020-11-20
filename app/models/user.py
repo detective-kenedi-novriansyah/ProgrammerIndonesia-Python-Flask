0e47558b32c73da6582b3109b392b9e5089a9e32
kenedi novriansyah <kenedinovriansyah@kenedis-MacBook-Air.local>
2020-11-21 04:09:42 +0700

    i will reinstall laptop

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,20 @@
-from app import db
+from app import db, login
+from flask_login import UserMixin
+from datetime import datetime
 
-class User(db.Model):
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(225), nullable=False)
+    email = db.Column(db.String(225), nullable=True)
     password = db.Column(db.String(225), nullable=False)
+    posts = db.relationship("Post", backref="author", lazy=True)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
---
+++ migrations/versions/03c61247fdee_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 03c61247fdee
+Revises: 73d93d6cb306
+Create Date: 2020-11-20 16:37:21.555798
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '03c61247fdee'
+down_revision = '73d93d6cb306'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
